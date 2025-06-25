b159b3ac89dfc635ef7273f719c1ed223d51fcc0
SaurabhIndi <saurabhyadavaligarh@gmail.com>
2025-06-26 01:13:40 +0530

    ASCII BST removeV

---
+++ Strings/asciiValue.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class ASCII{
+    public:
+    void value(){
+        char c = 'A';
+        cout<<"The ASCII value of "<<c<<" is : "<<int(c)<<endl;
+    }
+};
+int main(){
+    ASCII p1;
+    p1.value();
+
+    return 0;
+}
---
+++ Strings/removeVowels.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string remove(string s){
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            s=s.substr(0,i)+s.substr(i+1);
+            i--;
+        }
+    }
+    return s;
+}
+int main(){
+    string s = "take u forward";
+    cout<<remove(s)<<endl;
+}
